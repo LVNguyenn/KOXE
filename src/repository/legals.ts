b68e165d7737145266510c7c8bb601511635f747
caoqui <qui7d18@gmail.com>
2024-05-09 12:38:46 +0700

    fix description

--- src/repository/legals.ts
+++ src/repository/legals.ts
@@ -248,7 +248,7 @@ const LegalsRepository = {
         try {
             const processRepository = getRepository(Process)
             const processDb = await this.findProcessByIdSalonId(data);
-            const newProcess = await processRepository.save({ ...processDb?.data, name: data?.name });
+            const newProcess = await processRepository.save({ ...processDb?.data, name: data?.name, description: data?.description });
 
             return FormatData("success", "Updated the process successfully!", newProcess);
         } catch (error) {
