d7a73b856365bd04fc507643d2059f49d9e471b2
caoqui <qui7d18@gmail.com>
2024-07-12 12:34:58 +0700

    add mdw for legals

--- src/routes/legals.r.ts
+++ src/routes/legals.r.ts
@@ -6,57 +6,75 @@ const router = Router();
 
 router.post(
     "/create-process",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("C_PC"),
     legalsController.createProcess
 );
 
 router.post(
     "/create-documents",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("C_DC"),
     legalsController.createLegalDocuments
 );
 
 router.post(
     "/create-details",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("C_DL"),
     legalsController.createLegalDetails
 );
 
 router.post(
     "/process",
+    middlewareController.verifyToken2,
     legalsController.getAllProcess
 );
 
 router.patch(
     "/update-process",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("U_PC"),
     legalsController.updateProcess
 );
 
 router.patch(
     "/update-documents",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("U_DC"),
     legalsController.updateLegalDocuments
 );
 
 router.delete(
     "/delete-process",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("D_PC"),
     legalsController.removeProcess
 );
 
 router.delete(
     "/delete-documents",
+    middlewareController.verifyToken,
+    middlewareController.havePermission("D_DC"),
     legalsController.removeLegalDocuments
 );
 
 
 router.post(
     "/check-details-user",
+    middlewareController.verifyToken2,
     legalsController.addLegalDetailsForUser
 );
 
 router.post(
     "/get-legals-user",
+    middlewareController.verifyToken2,
     legalsController.getLegalsByPhoneCarId
 );
 
 router.patch(
     "/update-period-user",
+    middlewareController.verifyToken2,
     legalsController.updateNewPeriodForUser
 );
 
