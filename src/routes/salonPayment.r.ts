56e59576e73faa2cc1de02dc201292eb25ba658c
caoqui <qui7d18@gmail.com>
2024-07-12 12:39:20 +0700

    update mdw for sl pm

--- src/routes/salonPayment.r.ts
+++ src/routes/salonPayment.r.ts
@@ -7,15 +7,15 @@ const router = Router();
 
 // payment method salon
 router.post("/create-method", middlewareController.verifyToken, middlewareController.havePermission("C_PMT"), inforController.create);
-router.get("/method", middlewareController.verifyToken, inforController.get);
+router.get("/method", middlewareController.verifyToken2, inforController.get);
 router.patch("/method", middlewareController.verifyToken, middlewareController.havePermission("U_PMT"), inforController.update);
 router.delete("/method/:id", middlewareController.verifyToken, middlewareController.havePermission("D_PMT"), inforController.delete);
 
 router.post("/create", middlewareController.verifyToken, middlewareController.havePermission("C_PMSL"), payController.createPayment);
-router.post("/confirm-user", middlewareController.verifyToken, payController.confirmPaidFromUser);
+router.post("/confirm-user", middlewareController.verifyToken2, payController.confirmPaidFromUser);
 router.post("/confirm-salon", middlewareController.verifyToken, middlewareController.havePermission("U_PMSL"), payController.confirmPaidFromSalon);
 router.delete("/:id", middlewareController.verifyToken, middlewareController.havePermission("D_PMSL"), payController.delete);
-router.get("/", middlewareController.verifyToken, payController.get);
+router.get("/", middlewareController.verifyToken2, payController.get);
 
 
 export default router;
